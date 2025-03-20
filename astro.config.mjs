7b8cacc72a1dc67487683b28463b3d9b8bd84c6f
kiki <kariuki.marima@gmail.com>
2025-03-20 10:24:20 +0300

    updated config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
   integrations: [svelte()],
+  adapter: vercel(),
 });
